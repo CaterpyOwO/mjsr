9f21bb70f2559a4395d6c9ed197eceef8fa57d15
CaterpyOwO <mix3rxd@gmail.com>
2020-11-12 23:36:19 +0100

    updated serve.js for security reasons

--- serve.js
+++ serve.js
@@ -3,7 +3,7 @@ let port = 8080;
 let express = require("express");
 
 let app = express();
-app.use("/", express.static(__dirname + "/"));
+app.use("/", express.static(__dirname + "/src/"));
 app.listen(port, host);
 
 console.log("Running server at http://localhost:" + port + "/");
