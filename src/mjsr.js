eba4210d1ce8848d981dc00486ccde3035154c04
CaterpyOwO <mix3rxd@gmail.com>
2020-11-12 19:48:56 +0100

    Added simple example object

--- src/mjsr.js
+++ src/mjsr.js
@@ -4,13 +4,19 @@ import { Camera } from "./camera.js";
 import { Renderer } from "./renderer.js";
 import { Screen, Screen2d } from "./screen.js";
 
+import { default as Cube } from "./objects/cube.js";
+
 // import { shuffle } from "./math.js"
 
 let mjsr = {
 	Renderer,
     Screen,
     Screen2d,
-	Camera,
+    Camera,
+    
+    objects: {
+        Cube,
+    },
 };
 
 if (typeof define === 'function' && define.amd)
