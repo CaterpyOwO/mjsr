9f73d34f8e974ffbeea921b2a5b6e17ba80b3830
CaterpyOwO <mix3rxd@gmail.com>
2020-12-03 19:48:32 +0100

    come on

--- src/object/material.js
+++ src/object/material.js
@@ -3,6 +3,7 @@ import { parseColour } from "../utility/colour.js";
 export class Material {
 	constructor(colour, shinyness = 32.0) {
 		if (typeof colour == "string") this.colour = parseColour(colour);
+		// oh for fucks sake the lgtm alert isn't going away
 		else if (Array.isArray(colour)) this.colour = colour;
 		else throw new Error(`${colour} is not a valid colour.`);
 
