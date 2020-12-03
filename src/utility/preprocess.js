564dec62c2065bb3faee32be23d129da695c74fb
CaterpyOwO <mix3rxd@gmail.com>
2020-12-03 17:01:19 +0100

    fixed preprocessor

--- src/utility/preprocess.js
+++ src/utility/preprocess.js
@@ -1,38 +1,36 @@
 export function preprocess(source, options) {
-	let lines = source.split(/\n/),
-		lineCount = lines.length;
+    let lines = source.split(/\n/),
+        lineCount = lines.length;
 
-	let output = "";
+    let depths = {};
+    let output = "";
 
 	traverse();
 	return output.trim();
 
-	function traverse(currentLine = 0, condition = true, prevCond = true) {
-		while (currentLine < lineCount) {
-			let line = lines[currentLine];
 
-			if (line.trim().startsWith("#")) {
-				switch (line.trim().substr(1).split(/\s/)[0]) {
-					case "if":
-						prevCond = condition;
-						currentLine = traverse(
-							currentLine + 1,
-							eval(line.trim().substr(3)),
-							prevCond
-						);
-						break;
-					case "else":
-						currentLine = traverse(currentLine + 1, !condition, prevCond);
-						break;
-					case "endif":
-						currentLine = traverse(currentLine + 1, prevCond);
-						break;
-				}
-			} else if (condition) output += line + "\n";
+    function traverse(currentLine = 0, condition = true, depth = 0) {
+        while (currentLine < lineCount) {
+            let line = lines[currentLine];
 
-			currentLine++;
-		}
+            if (line.trim().startsWith("#")) {
+                switch (line.trim().substr(1).split(/\s/)[0]) {
+                    case "if":
+                        depths[depth] = condition;
+                        currentLine = traverse(currentLine + 1, eval(line.trim().substr(3)) && depths[depth], depth + 1);
+                    break;
+                    case "else":
+                        currentLine = traverse(currentLine + 1, !condition && depths[depth - 1], depth);
+                    break;
+                    case "endif":
+                        currentLine = traverse(currentLine + 1, depths[depth - 1], depth - 1);
+                    break;
+                }
+            } else if (condition) output += `${line}\n`;
+            
+            currentLine++;
+        }
 
-		return currentLine;
-	}
+        return currentLine;
+    }
 }
