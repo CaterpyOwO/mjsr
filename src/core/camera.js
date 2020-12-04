47bc499507556e384fccdcb4864243b640ab2b5f
CaterpyOwO <mix3rxd@gmail.com>
2020-12-04 15:43:44 +0100

    Drastically reduced bundle size

--- src/core/camera.js
+++ src/core/camera.js
@@ -1,39 +1,49 @@
-import * as mat4 from "../utility/gl-matrix/mat4.js";
+import {
+	create,
+	perspective,
+	rotateX,
+	rotateY,
+	translate,
+	invert,
+	transpose,
+	multiply,
+} from "../utility/gl-matrix/mat4.js";
 
 export class Camera {
-	constructor(position = [0, 0, 0], rotation = [0, 0, 0], fov = 6) {
+	constructor(position = [0, 0, 0], rotation = [0, 0, 0], fov = 45) {
 		this.pos = position;
 		this.rot = rotation;
+		this.fov = fov;
 
-		this.model = mat4.create();
-		this.view = mat4.create();
-		this.projection = mat4.create();
+		this.model = create();
+		this.view = create();
+		this.projection = create();
 
 		return this;
 	}
 
 	vp(canvas) {
-		let projection = mat4.perspective(
-			mat4.create(),
-			45 * (Math.PI / 180),
+		let projection = perspective(
+			create(),
+			this.fov * (Math.PI / 180),
 			canvas.width / canvas.height,
 			0.1,
 			100.0
 		);
 
 		// let view = mat4.translate(mat4.create(), mat4.create(), [0,0,-7])
-		let view = mat4.create();
-		mat4.rotateX(view, view, this.rot[0]);
-		mat4.rotateY(view, view, this.rot[1]);
-		mat4.translate(view, view, this.pos);
+		let view = create();
+		rotateX(view, view, this.rot[0]);
+		rotateY(view, view, this.rot[1]);
+		translate(view, view, this.pos);
 
-		return mat4.multiply(mat4.create(), projection, view);
+		return multiply(create(), projection, view);
 	}
 
 	modelit() {
 		let model = this.model;
-		mat4.invert(model, model);
-		mat4.transpose(model, model);
+		invert(model, model);
+		transpose(model, model);
 
 		return model;
 	}
