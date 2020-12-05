253d060575b0e786f7a0f7615659b404b75bc48f
CaterpyOwO <mix3rxd@gmail.com>
2020-12-05 23:00:04 +0100

    Added constants file

---
+++ src/core/constants.js
@@ -0,0 +1,9 @@
+// primitives
+export const POINTS = 0;
+export const LINES = 1;
+export const TRIANGLES = 2;
+
+//lighting
+export const NONE = 0;
+export const PHONG = 1;
+export const BLINN_PHONG = 2;
