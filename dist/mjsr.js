7da05742b036b83a455953259cf9bcc348679e24
CaterpyOwO <mix3rxd@gmail.com>
2021-01-24 13:46:59 +0100

    Build

--- dist/mjsr.js
+++ dist/mjsr.js
@@ -1426,7 +1426,7 @@ var mjsr = (function () {
 					let object = scene[o];
 					assert(typeof object == "object", "Invalid object in scene.");
 
-					if (!object instanceof Object3d) object = Object3d.from(object);
+					if (!(object instanceof Object3d)) object = Object3d.from(object);
 
 					let meshes = generateMesh(object);
 
