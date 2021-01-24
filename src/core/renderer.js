cc8a24775950a3341f6df3d7816960ff0d1b262a
CaterpyOwO <mix3rxd@gmail.com>
2021-01-24 23:49:50 +0100

    comma

--- src/core/renderer.js
+++ src/core/renderer.js
@@ -196,7 +196,7 @@ export class Renderer {
 			let model = this.scenes[this.__scene].objects[mesh.object].model;
 			if (model) shader.uniformMatrix4fv("u_modelobj", false, model);
 			//prettier-ignore
-			else shader.uniformMatrix4fv("u_modelobj", false, [1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,]);
+			else shader.uniformMatrix4fv("u_modelobj", false, [1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]);
 
 			let buffers = {
 				position: mesh.data.position,
